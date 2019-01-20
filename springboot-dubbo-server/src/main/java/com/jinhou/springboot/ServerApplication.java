39631b18899c41e4d28a3c44b7b5f7cf1e17745b
liujinhou <www.975764577@qq.com>
2019-01-20 16:04:48 +0800

    整合shiro

--- springboot-dubbo-server/src/main/java/com/jinhou/springboot/ServerApplication.java
+++ springboot-dubbo-server/src/main/java/com/jinhou/springboot/ServerApplication.java
@@ -1,13 +1,8 @@
 package com.jinhou.springboot;
 
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-
-import java.util.Arrays;
 
 /**
  * Spring Boot 应用启动类
@@ -26,18 +21,4 @@ public class ServerApplication {
     }
 
 
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
-
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
-            }
-
-        };
-    }
 }
